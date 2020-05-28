d6661392b5630b0aa5e547891f312e5b43e835f1
Ruth Ikegah <62059002+Ruth-ikegah@users.noreply.github.com>
2020-05-28 14:49:53 +0100

    Incomplete

--- Covid19.py
+++ Covid19.py
@@ -2,6 +2,7 @@
 #Problem:
 #I was not able to get the code to output the min, max, mean and sum of countries, it only outputs the particular cotinent details when the user selects.        
 
+# First block: User inputs desired continent and sees total cases for three countries in that continent
 
 continents = {"Africa": {"Nigeria":8344, "Ghana":6964, "South Africa":24268},
              "Europe": {"Uk":265227, "Russia":370680, "France":182722},
@@ -16,12 +17,15 @@ if continent in continents.keys():
         print(item, value)
              
 
-#After user sees cases in selected continent, program asks if user wants more details
+#Second block: After user sees cases in selected continent, program asks if user wants more details
 
 continent_details = {"Africa": {"Total cases":84123, "Active cases":45647, "Recovered cases":35788, "Total deaths":2287},
                "Europe": {"Total cases":1944334, "Active cases":847955, "Recovered cases":926548, "Total deaths":170061},
                "North America": {"Total cases":1953386, "Active cases":1221849, "Recovered cases":600758, "Total deaths":118405},
+                "Asia": {"Total cases":1044072, "Active cases":393020, "Recovered cases":601859, "Total deaths":29017},
+                 "South America": {"Total cases":697644, "Active cases":381531, "Recovered cases":275131,"Total deaths":34138}    
                }
+
 continents = input("Do you want to more details?, Enter the continent name ")
 
 continent_info = continent_details.get(continents, None)
@@ -34,5 +38,7 @@ if continent_info:
     print(f'Active cases: {continent_info["Active cases"]}')
     print(f'Recovered cases: {continent_info["Recovered cases"]}')
     print(f'Total deaths: {continent_info["Total deaths"]}\n')
- else:
-     print("Stop")
+else:
+    print("Stop")
+
+# This program contains data for 5 continents with 3 countries for each.          
